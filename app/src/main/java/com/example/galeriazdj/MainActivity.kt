db10ce12bc224df0a1f161040ac44dc172e40f60
JJacobT <tyszko0526@wp.pl>
2022-01-18 18:31:02 +0100

    dodane przyciski do obrotu i podpiete

--- app/src/main/java/com/example/galeriazdj/MainActivity.kt
+++ app/src/main/java/com/example/galeriazdj/MainActivity.kt
@@ -3,48 +3,55 @@ package com.example.galeriazdj
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    var obraz = 1;
+        var obraz = 1;
         findViewById<Button>(R.id.butn_cof).setOnClickListener {
-            if(obraz == 1)
+            if (obraz == 1)
                 obraz = 4;
             else
                 obraz -= 1;
-            if(obraz == 1 )
+            if (obraz == 1)
                 findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.domek);
-            else if(obraz == 2 )
+            else if (obraz == 2)
                 findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.auto);
-            else if(obraz == 3 )
+            else if (obraz == 3)
                 findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.piesek);
-            else if(obraz == 4 )
+            else if (obraz == 4)
                 findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.kotek);
 
         }
         findViewById<Button>(R.id.butn_dal).setOnClickListener {
             if (obraz == 4)
-                obraz = 1 ;
+                obraz = 1;
             else
-                obraz += 1 ;
-            if(obraz  == 1)
+                obraz += 1;
+            if (obraz == 1)
                 findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.domek);
-            else if(obraz == 2 )
+            else if (obraz == 2)
                 findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.auto);
-            else if(obraz == 3 )
+            else if (obraz == 3)
                 findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.piesek);
-            else if(obraz == 4 )
+            else if (obraz == 4)
                 findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.kotek);
         }
+        findViewById<Button>(R.id.butn_lewo).setOnClickListener {
+            var rotacja = "-" + findViewById<EditText>(R.id.rotacja).text;
+            findViewById<ImageView>(R.id.imageView).rotation = rotacja.toString().toFloat();
 
-
-
-
-
-
-
+        }
+        findViewById<Button>(R.id.butn_prawo).setOnClickListener {
+            var rotacja = "-" + findViewById<EditText>(R.id.rotacja).text;
+            findViewById<ImageView>(R.id.imageView).rotation = rotacja.toString().toFloat();
+        }
+        findViewById<Button>(R.id.skala).setOnClickListener {
+            var skala = findViewById<EditText>(R.id.skala).text;
+                findViewById<ImageView>(R.id.imageView).scaleX = skala.toString().toFloat()
+        }
     }
 }
