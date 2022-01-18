f6aebc32f1e15aca4877808462e2bd3da48d20b3
JJacobT <tyszko0526@wp.pl>
2022-01-18 18:07:27 +0100

    przypisany przycisk cofania sprawdzone działa

--- app/src/main/java/com/example/galeriazdj/MainActivity.kt
+++ app/src/main/java/com/example/galeriazdj/MainActivity.kt
@@ -9,7 +9,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    var obraz = 1;
+        findViewById<Button>(R.id.butn_cof).setOnClickListener {
+            if(obraz == 1)
+                obraz = 4;
+            else
+                obraz -= 1;
+            if(obraz == 1 )
+                findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.domek);
+            else if(obraz == 2 )
+                findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.auto);
+            else if(obraz == 3 )
+                findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.piesek);
+            else if(obraz == 4 )
+                findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.kotek);
 
+        }
 
 
 
